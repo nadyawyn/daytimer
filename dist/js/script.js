2fe24073ba1cfa5934e65b29f0609a4d1fcc1079
nadya <nadya.wyn@ya.ru>
2020-08-02 12:55:20 +0500

    Deleted console log

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,7 +14,6 @@ window.addEventListener('DOMContentLoaded', function () {
 	request.onload = function () {
 		let settingsList = request.response,
 			myActivities = settingsList.activities;
-		console.log(myActivities);
 		showActivities(myActivities);
 	};
 
